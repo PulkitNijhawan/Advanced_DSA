44ecc78f3adc5433b54f2b728de05bfb0bd1b72f
PulkitNijhawan <pulkitnijhawan098@gmail.com>
2020-12-28 15:29:07 +0530

    recursive tricks on trees(mark2 update)

--- Trees/mark2/basic.java
+++ Trees/mark2/basic.java
@@ -56,6 +56,7 @@ public class basic {
         if (root == null || root.data == k) {
             if (root == null)
                 return false;
+
             return true;
         }
         boolean res = false;
@@ -66,14 +67,157 @@ public class basic {
 
     public static int height(Node root) {
         if (root == null)
-            return 0;
+            return -1;
         return max(height(root.left), height(root.right)) + 1;
+    }
+
+    static ArrayList<Node> n2rp = new ArrayList<>();
+
+    public static boolean n2rp(Node root, int k) {
+        if (root == null || root.data == k) {
+            if (root != null && root.data == k) {
+                n2rp.add(root);
+                return true;
+            }
+            return false;
+        }
+        boolean res = n2rp(root.left, k) || n2rp(root.right, k);
+        if (res)
+            n2rp.add(root);
+        return res;
+    }
+
+    public static ArrayList<Node> n2rp02(Node root, int k) {
+        if (root == null || root.data == k) {
+            if (root != null && root.data == k) {
+                ArrayList<Node> baseRes = new ArrayList<>();
+                baseRes.add(root);
+                return baseRes;
+            }
+            return new ArrayList<>();
+        }
+        ArrayList<Node> left = n2rp02(root.left, k);
+        if (left.size() != 0) {
+            left.add(root);
+            return left;
+        }
+        ArrayList<Node> right = n2rp02(root.right, k);
+        if (right.size() != 0) {
+            right.add(root);
+            return right;
+        }
+        return new ArrayList<>();
+    }
+
+    public static Node lca(ArrayList<Node> one, ArrayList<Node> two) {
+        if (one.size() == 1 || two.size() == 1)
+            return one.get(one.size() - 1);
+        int i = one.size() - 1;
+        int j = two.size() - 1;
+        while (i != 0 && j != 0) {
+            if (one.get(i).data != two.get(j).data) {
+                break;
+            }
+            i--;
+            j--;
+        }
+        return one.get(i + 1);
+    }
+
+    static Node lcaNode = null;
+
+    public static boolean lca02(Node root, int a, int b) {
+        if (root == null)
+            return false;
+        boolean selfAns = false;
+        if (root.data == a || root.data == b) {
+            selfAns = true;
+        }
+        boolean rightAns = lca02(root.right, a, b);
+        if (lcaNode != null)
+            return true;
+        boolean leftAns = lca02(root.left, a, b);
+        if (lcaNode != null)
+            return true;
+        if ((selfAns && rightAns) || (selfAns && leftAns) || (leftAns && rightAns))
+            lcaNode = root;
+        return leftAns || rightAns || selfAns;
+    }
+
+    public static void kDown(Node rt, int level, Node blockNode, ArrayList<Node> ans) {
+        if (rt == null || rt == blockNode)
+            return;
+        if (level == 0) {
+            ans.add(rt);
+        }
+        kDown(rt.left, level - 1, blockNode, ans);
+        kDown(rt.right, level - 1, blockNode, ans);
+    }
+
+    public static void kfar01(Node root, int d, int k) {
+        ArrayList<Node> n2rp = n2rp02(root, d);
+        ArrayList<Node> ans = new ArrayList<>();
+        Node blockNode = null;
+        for (int i = 0; i < n2rp.size(); i++) {
+            kDown(n2rp.get(i), k - i, blockNode, ans);
+        }
+        System.out.println(ans);
+    }
+
+    public static int kfar02(Node root, int d, int k, ArrayList<Node> ans) {
+        if (root == null)
+            return -1;
+        if (root.data == d) {
+            kDown(root, k, null, ans);
+            return 1;
+        }
+        int leftAns = kfar02(root.left, d, k, ans);
+        if (leftAns > 0) {
+            if (k - leftAns >= 0)
+                kDown(root, k - leftAns, root.left, ans);
+            return leftAns + 1;
+        }
+        int rightAns = kfar02(root.right, d, k, ans);
+        if (rightAns > 0) {
+            if (k - rightAns >= 0)
+                kDown(root, k - rightAns, root.right, ans);
+            return rightAns + 1;
+        }
+        return -1;
+    }
+
+    public static int diameter(Node root) {
+        if (root == null)
+            return 0;
+        return max((height(root.left) + height(root.right) + 2), diameter(root.left), diameter(root.right));
+    }
+
+    static int diameter = -1;
+
+    public static int diameter02(Node root) {
+        if (root == null)
+            return -1;
+        int l = diameter02(root.left);
+        int r = diameter02(root.right);
+        diameter = max((l + r + 2), diameter);
+        return max(l, r) + 1;
     }
 
     public static void main(String[] args) {
         int[] arr = { 10, 20, 40, -1, -1, 50, -1, -1, 30, 60, 100, -1, -1, -1, 70, 110, -1, -1, 120, -1, -1 };
         Node root = constructTree(arr);
         // display(root);
-        System.out.println(height(root));
+        // System.out.println(n2rp(root, 100));
+        // for (Node x : n2rp) {
+        // System.out.print(x.data + " ");
+        // }
+        // ArrayList<Node> one = n2rp02(root, 70);
+        // ArrayList<Node> two = n2rp02(root, 70);
+        // ArrayList<Node> ans=new ArrayList<>();
+        // System.out.println(kfar02(root, 60, 3,ans));
+        // System.out.println(ans);
+
+        diameter02(root);
+        System.out.println(diameter);
     }
 }
