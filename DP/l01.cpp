1d0af3959221013664be96a3b90046be332e971b
PulkitNijhawan <pulkitnijhawan098@gmail.com>
2021-02-23 16:42:00 +0530

    string set

--- DP/l01.cpp
+++ DP/l01.cpp
@@ -2,6 +2,10 @@
 #include <vector>
 #include <list>
 using namespace std;
+#define mod 1000000007
+typedef long long int lli;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
 void print(vector<int> &dp)
 {
     for (int i : dp)
@@ -310,11 +314,45 @@ int dicePathDp(int N, vector<int> &dp)
         for (int i = 1; i <= 6; i++)
         {
             if (n - i >= 0)
-                cnt += dp[n-i];
+                cnt += dp[n - i];
         }
         dp[n] = cnt;
     }
     return dp[N];
+}
+int dicePathbtr(int N)
+{ //T:O(n) precise
+    //S:O(6)
+    list<int> ll;
+    ll.push_back(1);
+    int psum = 1;
+    for (int n = 1; n <= N; n++)
+    {
+        if (ll.size() < 6)
+        {
+            ll.push_back(psum);
+            psum += ll.back();
+        }
+        else
+        {
+            ll.push_back(psum);
+            psum += ll.back();
+            psum -= ll.front();
+            ll.pop_front();
+        }
+    }
+    return ll.back();
+}
+void climbstairs()
+{
+    int tar = 10;
+    vector<int> cost{3, 1, 5, 6, 8, 7};
+    vector<int> dp(tar + 1, -1);
+    // cout << climbStairsDp(tar, dp) << "\n";
+    // cout << minCostStairDp(cost, dp) << "\n";
+    // cout << dicePathDp(tar, dp) << "\n";
+    cout << dicePathbtr(10) << "\n";
+    print(dp);
 }
 void mazePath()
 {
@@ -325,27 +363,248 @@ void mazePath()
     cout << minPathSumDp(2, 2, dp, maze) << "\n";
     print2d(dp);
 }
-void climbstairs()
+long solve(string &s, int idx, vector<long> &dp)
 {
-    int tar = 10;
-    vector<int> cost{3, 1, 5, 6, 8, 7};
-    vector<int> dp(tar + 1, -1);
-    // cout << climbStairsDp(tar, dp) << "\n";
-    // cout << minCostStairDp(cost, dp) << "\n";
-    cout << dicePathDp(tar, dp) << "\n";
-    print(dp);
+    if (s[idx] == '0')
+        return dp[idx] = 0;
+    if (s.length() == idx)
+        return dp[idx] = 1;
+    if (dp[idx] != -1)
+        return dp[idx];
+    int cnt = 0;
+    if (s[idx] == '*')
+    {
+        for (int i = 1; i <= 9; i++)
+            cnt = ((cnt % mod) + (solve(s, idx + 1, dp) % mod)) % mod; //one call
+        if (s.length() - idx >= 2)
+        {
+            if (s[idx + 1] != '*')
+            {
+                int ch2 = s[idx + 1] - '0';
+                if (ch2 <= 6)
+                    cnt += (2 * (solve(s, idx + 2, dp) % mod)) % mod; //2calls
+                else
+                    cnt = ((cnt % mod) + (solve(s, idx + 2, dp) % mod)) % mod; //2 calls
+            }
+            else
+            {
+                for (int i = 1; i <= 15; i++)
+                    cnt = ((cnt % mod) + (solve(s, idx + 2, dp) % mod)) % mod; //2calls
+            }
+        }
+    }
+    else
+    {
+        int one = s[idx] - '0';
+
+        if (one != 0)
+            cnt = ((cnt % mod) + (solve(s, idx + 1, dp) % mod)) % mod; //one call
+        if (s.length() - idx >= 2)
+        {
+            if (s[idx + 1] != '*')
+            {
+                int two = (s[idx] - '0') * 10 + (s[idx + 1] - '0');
+                if (two > 9 && two <= 26)
+                    cnt = ((cnt % mod) + (solve(s, idx + 2, dp) % mod)) % mod; //two calls
+            }
+            else
+            {
+                if (one == 1)
+                    for (int i = 1; i <= 9; i++)
+                        cnt = ((cnt % mod) + (solve(s, idx + 2, dp) % mod)) % mod; //two calls
+                else if (one == 2)
+                    for (int i = 1; i <= 6; i++)
+                        cnt = ((cnt % mod) + (solve(s, idx + 2, dp) % mod)) % mod; //two calls
+            }
+        }
+    }
+    return dp[idx] = cnt;
+}
+void numDecodings(string s)
+{
+    vector<long> dp(s.length() + 1, -1);
+    cout << solve(s, 0, dp) << "\n";
+    for (long x : dp)
+        cout << x << " ";
 }
 void twoPointer()
 {
+    numDecodings("1*0*7*");
     // mazePath();
     // fibo();
-    climbstairs();
+    // climbstairs();
+}
+//===========================================================String Set==========================================================
+
+void palindromicSubstring(string str, vvi &dp)
+{
+    int n = str.length();
+    for (int gap = 0; gap < n; gap++)
+    {
+        for (int i = 0, j = gap; j < n; i++, j++)
+        {
+            if (gap == 0)
+                dp[i][j] = 1;
+            else if (gap == 1)
+                dp[i][j] = str[i] == str[j];
+            else
+                dp[i][j] = str[i] == str[j] && dp[i + 1][j - 1];
+        }
+    }
+}
+int longPalindromicSubstring(string str, vvi &dp)
+{
+    int n = str.length();
+    int ans = -1;
+    for (int gap = 0; gap < n; gap++)
+    {
+        for (int i = 0, j = gap; j < n; j++, i++)
+        {
+            if (gap == 0)
+                dp[i][j] = 1;
+            else if (gap == 1)
+                dp[i][j] = str[i] == str[j] ? 2 : 0;
+            else
+                dp[i][j] = str[i] == str[j] ? dp[i + 1][j - 1] + 2 : 0;
+            ans = max(ans, dp[i][j]);
+        }
+    }
+    return ans;
+}
+vi countPalindromicSubstring(string str, vvi &dp)
+{
+    vi ans(2);
+    int l = 0, h = 0;
+    int n = dp.size();
+    for (int gap = 0; gap < n; gap++)
+    {
+        for (int i = 0, j = gap; j < n; j++, i++)
+        {
+            ans[0] += dp[i][j];
+            if (dp[i][j])
+            {
+                ans[1] = j - i + 1;
+                h = j;
+                l = i;
+            }
+        }
+    }
+    cout << str.substr(l, h - l + 1) << "\n";
+    return ans;
+}
+int longPalindromicSubseq(string str, int i, int j, vvi &dp)
+{
+    if (i > j)
+        return dp[i][j] = 0;
+    if (i == j)
+        return dp[i][j] = 1;
+    if (dp[i][j] != 0)
+        return dp[i][j];
+    int tendency = -1;
+    if (str[i] == str[j])
+        tendency = longPalindromicSubseq(str, i + 1, j - 1, dp) + 2;
+    else
+    {
+        tendency = max(tendency, longPalindromicSubseq(str, i + 1, j, dp));
+        tendency = max(tendency, longPalindromicSubseq(str, i, j - 1, dp));
+    }
+    return dp[i][j] = tendency;
+}
+int longPalindromicSubseqDp(string str, vvi &dp)
+{
+    int n = str.length();
+    for (int gap = 0; gap < n; gap++)
+    {
+        for (int i = 0, j = gap; j < n; i++, j++)
+        {
+            if (i == j)
+            {
+                dp[i][j] = 1;
+                continue;
+            }
+            int tendency = -1;
+            if (str[i] == str[j])
+                tendency = dp[i + 1][j - 1] + 2;
+            else
+            {
+                tendency = max(tendency, dp[i + 1][j]);
+                tendency = max(tendency, dp[i][j - 1]);
+            }
+            dp[i][j] = tendency;
+        }
+    }
+    return dp[0][n - 1];
+}
+// int longPalindromicSubseq_btr(string str, int i, int j, vvi &dp, vvi &dpstr)
+// {
+//     if (i > j)
+//         return dp[i][j] = 0;
+//     if (i == j)
+//         return dp[i][j] = 1;
+//     if (dpstr[i][j] != 0)
+//         return dp[i][j] = dpstr[i][j];                             //FIND RIGHT SOLUTION
+//     if (dp[i][j] != 0)
+//         return dp[i][j];
+//     int tendency = -1;
+//     if (str[i] == str[j])
+//         tendency = longPalindromicSubseq_btr(str, i + 1, j - 1, dp, dpstr);
+//     else
+//     {
+//         tendency = max(tendency, longPalindromicSubseq_btr(str, i + 1, j, dp, dpstr));
+//         tendency = max(tendency, longPalindromicSubseq_btr(str, i, j - 1, dp, dpstr));
+//     }
+//     return tendency;
+// }
+int distinctSubseq(string &s, string &t, int n, int m, vvi &dp)
+{
+    if (m > n)
+        return dp[n][m] = 0;
+    if (m == 0)
+        return dp[n][m] = 1;
+    if (dp[n][m] != -1)
+        return dp[n][m];
+    int tendency = 0;
+    if (s[n] == t[m])
+    {
+        tendency += distinctSubseq(s, t, n - 1, m - 1, dp);
+        tendency += distinctSubseq(s, t, n - 1, m, dp);
+    }
+    else
+    {
+        tendency += distinctSubseq(s, t, n - 1, m, dp);
+    }
+    return dp[n][m] = tendency;
+}
+void palindromics()
+{
+    // string str = "abcbddbcaab";
+    // int n = str.length();
+    // vvi dpstr(n, vi(n, 0));
+    // longPalindromicSubstring(str, dpstr);
+    // vi ans = countPalindromicSubstring(str, dpstr);
+    // print(ans);
+    // print2d(dpstr);
+    // vvi dp(n, vi(n, 0));
+    // cout << longPalindromicSubseqDp(str, dp) << "\n";
+    // cout << longPalindromicSubseq_btr(str, 0, n - 1, dp, dpstr) << "\n";
+    // print2d(dp);
+    string s = "rabbbit", t = "rabbit";
+    int n = s.length(), m = t.length();
+    vvi dp(n, vi(m, -1));
+    cout << distinctSubseq(s, t, n - 1, m - 1, dp) << "\n";
+    print2d(dp);
+}
+
+void stringSet()
+{
+    palindromics();
 }
 
 void solve()
 {
     // g++ l01.cpp -o p && ./p > output.txt
-    twoPointer();
+    // twoPointer();
+    stringSet();
 }
 int main()
 {
